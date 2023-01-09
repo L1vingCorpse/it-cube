c7f01c1357ef608698cfad263bfca0aa70581c48
L1vingCorpse <122265326+L1vingCorpse@users.noreply.github.com>
2023-01-09 18:18:44 +0800

    Add files via upload

---
+++ 09.01 test/task3.py
@@ -0,0 +1,9 @@
+m=input()
+a=int(input())
+if m=="f":
+    if a<=15 and a>=10:
+        print("ye")
+    else:
+        print("no")
+else:
+    print("no")
---
+++ 09.01 test/task4.py
@@ -0,0 +1,25 @@
+n=int(input())
+if n<=10:
+    if n==1:
+        print("I")
+    if n==2:
+        print("II")
+    if n==3:
+        print("III")
+    if n==4:
+        print("IV")
+    if n==5:
+        print("V")
+    if n==6:
+        print("VI")
+    if n==7:
+        print("VII")
+    if n==8:
+        print("VIII")
+    if n==9:
+        print("IX")
+    if n==10:
+        print("X")
+else:
+    print("error..."
+          "please wait...")
